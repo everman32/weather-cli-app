4d89a929647ebf1e5c5e3b84c0e4fe35fc03f869
Victor <vitek.borodyn@gmail.com>
2022-08-07 02:08:39 +0300

    Add read and write token

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 
 import getOptions from "./services/options.js";
+import { printSuccess, printError } from "./services/log.js";
+import { writeKeyValue } from "./services/storage.js";
+
+const saveToken = async (token) => {
+  try {
+    await writeKeyValue("token", token);
+    printSuccess("Token saved");
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
 const initApp = () => {
   const options = getOptions();
   console.log(options);
+  if (options.token) {
+    return saveToken(options.token);
+  }
+  return 0;
 };
 
 initApp();
