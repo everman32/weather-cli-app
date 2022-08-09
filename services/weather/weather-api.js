ad3b0c3082d89dadd724f13723d7ab1841b218bd
Victor <vitek.borodyn@gmail.com>
2022-08-10 01:02:08 +0300

    Add api returning weather by coordinates

---
+++ services/weather/weather-api.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const getWeatherByCoordinates = async (lat, lon, appid, units) => {
+  const { data } = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      params: {
+        lat,
+        lon,
+        appid,
+        units,
+      },
+    }
+  );
+  return data;
+};
+
+export default getWeatherByCoordinates;
