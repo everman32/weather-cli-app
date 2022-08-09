d704a7067e2b82513585537ef553de5dddd9e8c4
Victor <vitek.borodyn@gmail.com>
2022-08-09 22:05:27 +0300

    Add work with config

--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 #!/usr/bin/env node
 
 import getOptions from "./services/options.js";
-import { printSuccess, printError } from "./services/log.js";
-import { writeKeyValue } from "./services/storage.js";
+import { printSuccess, printError, printWeather } from "./services/printer.js";
+import {
+  readValueFromFile,
+  writeObjectToFile,
+} from "./services/weather/config-io.js";
+import getCoordinatesByCity from "./services/weather/geocoding-api.js";
+import getWeatherByCoordinates from "./services/weather/weather-api.js";
+
+const setConfig = async (object) => {
+  try {
+    if ((await writeObjectToFile(object)) === 0) {
+      printSuccess(`${Object.keys(object)[0]} saved`);
+    }
+  } catch (e) {
+    printError(e.message);
+  }
+};
+
+const getConfig = async (object) => {
+  try {
+    const key = Object.keys(object)[0];
+    const value = await readValueFromFile(key);
+    return value;
+  } catch (e) {
+    printError(e.message);
+    return 0;
+  }
+};
+
+const resolveConfig = async (object) => {
+  const key = Object.keys(object)[0];
+  let value = object[key];
+
+  if (value) {
+    await setConfig(object);
+  } else {
+    value = await getConfig(object);
+
+    if (value === undefined) {
+      printError(`${key} is not defined`);
+    }
+  }
+  return value;
+};
 
 const saveToken = async (token) => {
   try {
