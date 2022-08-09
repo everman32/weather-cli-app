865a968407e8b4ddc784bb32458ce0954d05d837
Victor <vitek.borodyn@gmail.com>
2022-08-10 01:04:01 +0300

    Rename log.js and add weather print method

---
+++ services/printer.js
@@ -0,0 +1,24 @@
+import chalk from "chalk";
+import getIcon from "./weather/icons.js";
+
+const printSuccess = (successMessage) => {
+  console.log(`${chalk.bgGreen(" SUCCESS ")} ${successMessage}`);
+};
+
+const printError = (errorMessage) => {
+  console.error(`${chalk.bgRed(" ERROR ")} ${errorMessage}`);
+};
+
+const printWeather = (weather, descriptions) => {
+  console.log(
+    `${chalk.bgMagenta(" WEATHER ")} Weather in ${weather.name}
+  ${getIcon(weather.weather[0].icon)}  ${weather.weather[0].description}
+  Temperature: ${weather.main.temp}${descriptions.temp} (Feels like ${
+      weather.main.feels_like
+    }${descriptions.temp})
+  Humidity: ${weather.main.humidity}${descriptions.percent}
+  Wind speed: ${weather.wind.speed}${descriptions.speed}`
+  );
+};
+
+export { printSuccess, printError, printWeather };
